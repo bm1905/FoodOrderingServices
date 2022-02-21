653106909cc53e87b649099e0d569022b46a854a
bm1905 <bijay.maharjan5@gmail.com>
2022-02-20 23:46:10 -0600

    Refactor for versioning and integration test

---
+++ src/BuildingBlocks/Common.Exceptions/BadRequestException.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace Catalog.API.Helpers.Exceptions
+{
+    public class BadRequestException : System.Exception
+    {
+        public BadRequestException() : base() { }
+
+        public BadRequestException(string message) : base(message) { }
+
+        public BadRequestException(string message, params object[] args)
+            : base(string.Format(CultureInfo.CurrentCulture, message, args))
+        {
+        }
+    }
+}
